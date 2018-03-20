101a54437e759453afd0646fb09aebdb4077b02f
Ali Emir Sen <senaliemir@gmail.com>
2018-03-20 22:26:56 +0300

    remove and update functions added

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-//remove todo from list
-function removeTodo(e) {}
-
 //save todos
 function saveTodo() {
   var newInput = document.getElementById('myInput').value;
@@ -16,14 +13,12 @@ function saveTodo() {
   };
 
   if (localStorage.getItem('todos') === null) {
-    console.log('null');
     var todos = [];
 
     todos.push(todo);
 
     localStorage.setItem('todos', JSON.stringify(todos));
   } else {
-    console.log('notnull');
     var todos = JSON.parse(localStorage.getItem('todos'));
     todos.push(todo);
     localStorage.setItem('todos', JSON.stringify(todos));
@@ -46,18 +41,60 @@ function fetchTodos() {
     var todo = document.createTextNode(todos[i].text);
     li.appendChild(todo);
     ul.appendChild(li);
+    li.addEventListener(
+      'click',
+      function() {
+        updateTodo(this);
+      },
+      false
+    );
 
     //X button
     var span = document.createElement('SPAN');
     var txt = document.createTextNode('\u00D7');
     span.className = 'close';
-    span.onclick = 'removeTodo()';
+    span.addEventListener(
+      'click',
+      function() {
+        removeTodo(this.parentElement.innerText);
+      },
+      false
+    );
     span.appendChild(txt);
     li.appendChild(span);
 
     //checked
     if (todos[i].checked) {
       li.classList.toggle('checked');
+    }
+  }
+}
+//remove todo from list
+function removeTodo(tx) {
+  var removingTodo = tx.substring(0, tx.length - 1);
+  var todos = JSON.parse(localStorage.getItem('todos'));
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i].text == removingTodo) {
+      todos.splice(i, 1);
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  }
+  fetchTodos();
+}
+//update Todo
+function updateTodo(li) {
+  li.classList.toggle('checked');
+  var updatingTodo = li.innerText.substring(0, li.innerText.length - 1);
+  console.log(updatingTodo);
+  var todos = JSON.parse(localStorage.getItem('todos'));
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i].text == updatingTodo) {
+      if (todos[i].checked) {
+        todos[i].checked = false;
+      } else {
+        todos[i].checked = true;
+      }
+      localStorage.setItem('todos', JSON.stringify(todos));
     }
   }
 }
